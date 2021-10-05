e38dc26c21e6dc2914d659c5782f84334d4d1548
ialexies <ialexies@gmail.com>
2021-10-05 14:51:59 +0800

    🎉 💄 🎨  Finalized Design

--- lib/encode/view/encode_view.dart
+++ lib/encode/view/encode_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:bordered_text/bordered_text.dart';
 
 class EncodeView extends StatefulWidget {
   const EncodeView({Key? key}) : super(key: key);
@@ -16,7 +17,7 @@ class _EncodeViewState extends State<EncodeView> {
         child: Stack(
           children: [
             Opacity(
-              opacity: .6,
+              opacity: .4,
               child: Padding(
                 padding: const EdgeInsets.only(top: 150),
                 child: Container(
@@ -37,22 +38,26 @@ class _EncodeViewState extends State<EncodeView> {
               child: Stack(
                 children: [
                   Container(
-                    // width: MediaQuery.of(context).size.width,
-                    height: MediaQuery.of(context).size.height,
-                    decoration: BoxDecoration(
+                    // color: Colors.white,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      boxShadow: [
+                        BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 6,
+                            spreadRadius: 6)
+                      ],
+                      // border: Border.all(color: Colors.black, width: 3),
                       borderRadius: BorderRadius.only(
                           bottomLeft: Radius.circular(50),
                           bottomRight: Radius.circular(50)),
-                      border: Border.all(color: Colors.black, width: 3),
-                      image: DecorationImage(
-                        fit: BoxFit.fitHeight,
-                        image: AssetImage('images/header_clouds.jpeg'),
-                      ),
                     ),
                   ),
                   Container(
-                    decoration: const BoxDecoration(
-                      gradient: LinearGradient(
+                    // color: Colors.white,
+                    decoration: BoxDecoration(
+                      // color: Colors.white,
+                      gradient: const LinearGradient(
                           begin: Alignment.topCenter,
                           end: Alignment.bottomCenter,
                           colors: [
@@ -60,25 +65,47 @@ class _EncodeViewState extends State<EncodeView> {
                             Color(0xC27EDDFF),
                             Color(0xDE84F082),
                           ]),
-                      borderRadius: BorderRadius.only(
+                      // boxShadow: [BoxShadow(color: Colors.black)],
+                      border: Border.all(color: Color(0xff00698F), width: 3),
+                      borderRadius: const BorderRadius.only(
                           bottomLeft: Radius.circular(50),
                           bottomRight: Radius.circular(50)),
+                    ),
+                  ),
+                  Opacity(
+                    opacity: .3,
+                    child: Container(
+                      // width: MediaQuery.of(context).size.width,
+                      height: MediaQuery.of(context).size.height,
+                      decoration: const BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(50),
+                            bottomRight: Radius.circular(50)),
+                        image: DecorationImage(
+                          fit: BoxFit.fitHeight,
+                          image: AssetImage('images/header_clouds.jpeg'),
+                        ),
+                      ),
                     ),
                   ),
                   Center(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.center,
-                      children: const [
-                        Text(
-                          'CAESAR CIPHER',
-                          style: TextStyle(
-                            color: Color(0xFF006887),
-                            fontWeight: FontWeight.bold,
-                            fontSize: 30,
+                      children: [
+                        BorderedText(
+                          strokeWidth: 2,
+                          strokeColor: Colors.white,
+                          child: const Text(
+                            'CAESAR CIPHER',
+                            style: TextStyle(
+                              color: Color(0xFF006887),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30,
+                            ),
                           ),
                         ),
-                        Text(
+                        const Text(
                           '[ Generate & Encrypt a word]',
                           style: TextStyle(
                             color: Colors.white,
@@ -97,14 +124,72 @@ class _EncodeViewState extends State<EncodeView> {
               child: Column(
                 children: [
                   Container(
-                    child: Text('fdf'),
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 20,
+                      ),
+                      child: Expanded(
+                        child: Column(
+                          children: [
+                            Row(
+                              children: [
+                                ElevatedButton(
+                                  style: ElevatedButton.styleFrom(
+                                    padding: const EdgeInsets.all(8),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(12),
+                                    ),
+                                    side: const BorderSide(color: Colors.white),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Text(
+                                    'Generate Phrase',
+                                    style: TextStyle(fontSize: 10),
+                                  ),
+                                ),
+                                const SizedBox(
+                                  width: 10,
+                                ),
+                                const Center(
+                                  child: Text(
+                                    'Words: 4',
+                                    style: TextStyle(fontSize: 14),
+                                  ),
+                                )
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Expanded(
+                                  child: TextField(
+                                    minLines: 3,
+                                    maxLines: 8,
+                                    keyboardType: TextInputType.multiline,
+                                    // maxLines: null,
+                                    decoration: InputDecoration(
+                                      filled: true,
+                                      fillColor: Colors.white70,
+                                      border: OutlineInputBorder(
+                                        borderSide:
+                                            new BorderSide(color: Colors.white),
+                                      ),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
                     margin: EdgeInsets.only(top: 180),
-                    height: 200,
+                    // height: 200,
                     width: 250,
                     decoration: BoxDecoration(
                       // color: Colors.blue,
                       color: const Color.fromRGBO(15, 211, 223, 1),
-                      borderRadius: const BorderRadius.all(Radius.circular(50)),
+                      borderRadius: const BorderRadius.all(Radius.circular(20)),
                       border: Border.all(
                         color: Colors.white.withOpacity(.5),
                         width: 3,
--- lib/home/view/home_view.dart
+++ lib/home/view/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:caesarcipher/encode/view/encode_view.dart';
 import 'package:caesarcipher/validate/view/validated_view.dart';
 import 'package:flutter/material.dart';
-import 'package:liquid_swipe/liquid_swipe.dart';
+// import 'package:liquid_swipe/liquid_swipe.dart';
 
 class MyHomePage extends StatefulWidget {
   // const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -40,27 +40,48 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       // appBar: AppBar(
       //   title: Text('CAESAR CIPHER'),
       // ),
       body: PageView(
         controller: _controller,
         // physics: BouncingScrollPhysics(),
+        physics: NeverScrollableScrollPhysics(),
         children: pages,
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: this.pageIndex,
-        onTap: ontap,
-        items: const [
-          BottomNavigationBarItem(
-            label: 'Encrypt',
-            icon: Icon(Icons.ac_unit),
+
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+          boxShadow: [
+            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
           ),
-          BottomNavigationBarItem(
-            label: 'Decrypt',
-            icon: Icon(Icons.ac_unit),
+          child: BottomNavigationBar(
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Colors.amber[800],
+            backgroundColor: Colors.amber,
+            currentIndex: this.pageIndex,
+            onTap: ontap,
+            items: const [
+              BottomNavigationBarItem(
+                label: 'Encrypt',
+                icon: Icon(Icons.ac_unit),
+              ),
+              BottomNavigationBarItem(
+                label: 'Decrypt',
+                icon: Icon(Icons.ac_unit),
+              ),
+            ],
           ),
-        ],
+        ),
       ),
     );
 
